6333020f8f4b2e4fdf2f63e2e388b34569808536
Cleiton <cleitonrmacedo@gmail.com>
2022-12-06 16:52:46 -0300

    feat: Requisito 3 - Implementado funcao get_unique_industries que retorna um conjunto de valores unicos presentes na coluna industry do arquivo em data/jobs.csv

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,14 @@
 from typing import List, Dict
+from .jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    csvfile = read(path)
+    industries = set()
+    for industry in csvfile:
+        industries.add(industry['industry'])
+    return industries.difference({''})
+# retorna {'Retail', 'Business Services', 'Non-Profit', 'Health Care' e mais
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
