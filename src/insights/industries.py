4901b08da9d12337bc10234d10d80cb158dbe641
Cleiton <cleitonrmacedo@gmail.com>
2022-12-06 19:38:11 -0300

    refactor: funcao filter_by_industry em industries.py refatorado para list comprehension

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -12,11 +12,11 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    industry_list = []
-    for job in jobs:
-        if job['industry'] == industry:
-            industry_list.append(job)
-    return industry_list
+    # industry_list = []
+    # for job in jobs:
+    #     if job['industry'] == industry:
+    #         industry_list.append(job)
+    return [job for job in jobs if job['industry'] == industry]
 # retorno de exemplo = [
 #     {'id': 1, 'industry': 'agriculture'},
 #     {'id': 2, 'industry': 'agriculture'}
