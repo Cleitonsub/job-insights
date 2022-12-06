63b3f501d6a443d23d1a06be038dac859e25363a
Cleiton <cleitonrmacedo@gmail.com>
2022-12-06 17:44:20 -0300

    feat: Requisito 6 - Implementado funcao filter_by_job_type que retorna uma lista com todos os empregos onde a coluna job_type corresponde ao parametro job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -25,18 +25,12 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    job_types_list = []
+    for job in jobs:
+        if job['job_type'] == job_type:
+            job_types_list.append(job)
+    return job_types_list
+# retorno de exemplo = [
+#       {'id': 1, 'job_type': 'PART_TIME'},
+#       {'id': 2, 'job_type': 'PART_TIME'}
+# ]
