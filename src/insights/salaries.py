127be37f378c57d5d60851ae1944667185d00d2c
Cleiton <cleitonrmacedo@gmail.com>
2022-12-08 17:34:43 -0300

    feat: Requisito 8 - Implementado funcao matches_salary_range que retorna True se o salario procurado estiver dentro da faixa salarial

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -27,29 +27,18 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    if (
+        ('min_salary' or 'max_salary') not in job
+        or type(job.get('min_salary')) is not int
+        or type(job.get('max_salary')) is not int
+        or job['min_salary'] > job['max_salary']
+        or not str(salary).lstrip('-').isdigit()
+    ):
+        raise ValueError
+    return job['min_salary'] <= int(salary) <= job['max_salary']
+# test = matches_salary_range({"max_salary": 10000, "min_salary": 200}, -1000)
+# print(test)
+# .lstrip('-') remove o '-' caso haja a esquerda do numero
 
 
 def filter_by_salary_range(
